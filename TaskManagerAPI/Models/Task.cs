077032f93782137ec4dc741befd4e62ff843b1f3
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-10-24 20:31:13 +0530

    small update

--- TaskManagerAPI/Models/Task.cs
+++ TaskManagerAPI/Models/Task.cs
@@ -3,7 +3,7 @@ namespace TaskManagerAPI.Models;
 public class Task
 {
     public int Id { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
     public int IsCompleted { get; set; }
 }
