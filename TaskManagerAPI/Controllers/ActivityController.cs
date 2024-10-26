734d26230282ea774e94d0ca4305c19750ae3089
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-10-26 15:01:02 +0530

    install identity nuget packages.

--- TaskManagerAPI/Controllers/ActivityController.cs
+++ TaskManagerAPI/Controllers/ActivityController.cs
@@ -67,8 +67,4 @@ public class ActivityController: ControllerBase
         else
             return NotFound();
     }
-
-
-
-
 }
--- TaskManagerAPI/obj/Debug/net8.0/TaskManagerAPI.AssemblyInfo.cs
+++ TaskManagerAPI/obj/Debug/net8.0/TaskManagerAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+758b146f688de458e720e87482c8634de896f9a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5b2a2a390545b5f89f16e4320c715a9cd31bee3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
