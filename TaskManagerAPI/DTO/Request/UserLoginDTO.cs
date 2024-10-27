efa56eb9f38617b3930948578cea339f3d8fdba1
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-10-27 08:30:27 +0530

    create the dto's to register user and login.

---
+++ TaskManagerAPI/DTO/Request/UserLoginDTO.cs
@@ -0,0 +1,7 @@
+namespace TaskManagerAPI.DTO.Request;
+
+public class UserLoginDTO
+{
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+}
---
+++ TaskManagerAPI/DTO/Request/UserRegisterDTO.cs
@@ -0,0 +1,8 @@
+namespace TaskManagerAPI.DTO.Request;
+
+public class UserRegisterDTO
+{
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public string? Role { get; set; }
+}
